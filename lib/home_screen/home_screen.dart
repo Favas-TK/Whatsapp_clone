12562c13878f3bc2798d5c984b043f48810c657b
Favas-TK <mhdfavastk@gmail.com>
2022-10-30 11:18:11 +0530

    name changed to old

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -25,7 +25,7 @@ class _HomeScreenState extends State<HomeScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('WhatsApp Beta'),
+        title: const Text('WhatsApp'),
         actions: [
           const Icon(Icons.search),
           PopupMenuButton(
